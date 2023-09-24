8e75df928d1e523123b5d7e8c62a036f927cd0f3
Jpanda2000 <jajatipanda6@gmail.com>
2023-09-24 10:48:11 +0530

    this is my first commit

---
+++ assignment_2/rock_paper_scissor.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.util.Random;
+class rock_paper_scissor
+{
+    public static void main(String arg[])
+    {
+        Scanner sc=new Scanner(System.in);
+        Random rnd=new Random();
+        String choices[]={"rock","paper","scissor"};
+        while(true)
+        {
+            System.out.println("enter your choice");
+            String userchoice=sc.nextLine();
+            if
+        }
+    }
+}
